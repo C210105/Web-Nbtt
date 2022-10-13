62f4f00a1db59415d06eb5b77361cba536f4d77e
DANGLONG <long.bmw10@gmail.com>
2022-10-14 03:34:41 +0900

    tao sua doi

---
+++ src/main/java/nbtt/com/vn/webnbtt/beans/SanPham.java
@@ -0,0 +1,15 @@
+package nbtt.com.vn.webnbtt.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SanPham {
+ 
+    private String maSP; // ID
+
+    private String loaiSP; // 種類
+}
